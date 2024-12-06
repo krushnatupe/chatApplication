47744fef75debe4f28d6fe65f1c891614ad38e6f
krushnatupe <103983697+krushnatupe@users.noreply.github.com>
2024-12-06 09:12:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://quickchat-k54q.onrender.com',
     credentials:true
 };
 app.use(cors(corsOption)); 
